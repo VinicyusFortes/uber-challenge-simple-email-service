bbbd934857fd88b25dd2d7c23d043224db80bf7b
VinicyusFortes <vinicyusfortess@gmail.com>
2025-07-23 11:20:41 +0100

    Add SesEmailSender.java in infrastructure layer

--- src/main/java/com/fortes/email_service/core/EmailServiceUseCase.java
+++ src/main/java/com/fortes/email_service/core/EmailServiceUseCase.java
@@ -1,5 +1,7 @@
 package com.fortes.email_service.core;
 
+import org.springframework.stereotype.Component;
+
 //TODO add documentation
 public interface EmailServiceUseCase {
   void sendEmail(String to, String sibject, String body);
